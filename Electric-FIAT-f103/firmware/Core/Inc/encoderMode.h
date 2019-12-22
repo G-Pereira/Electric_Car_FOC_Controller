87d976cb0fc27e2c9e3db596adfeae77429b207e
Unknown <up201505136@fe.up.pt>
2019-12-22 20:16:14 +0000

    projeto de teste com f103

---
+++ Electric-FIAT-f103/firmware/Core/Inc/encoderMode.h
@@ -0,0 +1,12 @@
+/*
+ * encoderMode.h
+ *
+ *  Created on: Dec 18, 2019
+ */
+
+#ifndef INC_ENCODERMODE_H_
+#define INC_ENCODERMODE_H_
+
+float motorSpeed (uint32_t cnt1, TIM_HandleTypeDef htim3);
+
+#endif /* INC_ENCODERMODE_H_ */
---
+++ Electric-FIAT-f103/firmware/Core/Inc/main.h
@@ -0,0 +1,93 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_Pin GPIO_PIN_13
+#define LED_GPIO_Port GPIOC
+#define encoder_va_Pin GPIO_PIN_6
+#define encoder_va_GPIO_Port GPIOA
+#define encoder_vb_Pin GPIO_PIN_7
+#define encoder_vb_GPIO_Port GPIOA
+#define SDCard_CS_Pin GPIO_PIN_10
+#define SDCard_CS_GPIO_Port GPIOB
+#define SDCard_Detect_Pin GPIO_PIN_11
+#define SDCard_Detect_GPIO_Port GPIOB
+#define Clock_Output_Pin GPIO_PIN_8
+#define Clock_Output_GPIO_Port GPIOA
+#define FOC_CS_Pin GPIO_PIN_9
+#define FOC_CS_GPIO_Port GPIOA
+#define CS_magnet_Pin GPIO_PIN_10
+#define CS_magnet_GPIO_Port GPIOA
+#define CS_gyro_Pin GPIO_PIN_11
+#define CS_gyro_GPIO_Port GPIOA
+#define CS_accel_Pin GPIO_PIN_12
+#define CS_accel_GPIO_Port GPIOA
+#define CAN_Stand_By_Pin GPIO_PIN_15
+#define CAN_Stand_By_GPIO_Port GPIOA
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
